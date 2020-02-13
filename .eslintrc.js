4cd0dbfc81bc85275adf1e0c5eb9e87b31873afb
Changwan Jun <wan2land@gmail.com>
2020-02-13 18:00:16 +0900

    Init monorepo

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    jest: true,
+  },
+  extends: [
+    '@stdjs',
+    '@stdjs/eslint-config/typescript',
+  ],
+  ignorePatterns: [
+    'node_modules/',
+  ],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+}
---
+++ packages/airframe/src/index.ts
@@ -0,0 +1,2 @@
+
+export const AIRFRAME = true
